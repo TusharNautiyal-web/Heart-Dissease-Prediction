836086929b21b5983fa056f97dd94290ce80d1a6
tushar nautiyal <tsa.vevo.music@gmail.com>
2022-06-07 15:07:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -124,4 +124,4 @@ def predict():
         return render_template('Index.html')
 
 if __name__=="__main__":
- app.run(host="localhost", port=8080, debug=True)
+ app.run(host="0.0.0.0", port=8000, debug=True)
